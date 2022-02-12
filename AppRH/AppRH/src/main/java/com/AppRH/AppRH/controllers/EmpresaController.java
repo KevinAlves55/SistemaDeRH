f26940350eac5feac742d638ad44ced33bbd930b
INF1B-EAD <INF1B-EAD@cfp127aluno.local>
2022-02-12 11:51:39 -0300

    Sistema com Spring Boot

---
+++ AppRH/AppRH/src/main/java/com/AppRH/AppRH/controllers/EmpresaController.java
@@ -0,0 +1,91 @@
+package com.AppRH.AppRH.controllers;
+
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.AppRH.AppRH.models.Empresa;
+import com.AppRH.AppRH.repository.EmpresaRepository;
+
+@Controller
+public class EmpresaController {
+	
+	@Autowired
+	private EmpresaRepository er;
+	
+	// CADASTRAR EMPRESA
+	@RequestMapping(value = "/cadastrarEmpresa", method = RequestMethod.GET)
+	public String form() {
+		
+		return "empresa/form-empresa";
+		
+	}
+	
+	@RequestMapping(value = "/cadastrarEmpresa", method = RequestMethod.POST)
+	public String form(@Valid Empresa empresa, BindingResult result, RedirectAttributes attributes) {
+		
+		if (result.hasErrors()) {
+			
+			attributes.addFlashAttribute("mensagem", "Verifique os campos");
+			return "redirect:/cadastrarEmpresa";
+			
+		}
+		
+		er.save(empresa);
+		attributes.addFlashAttribute("mensagem", "Empresa cadastrada com sucesso");
+		return "redirect:/cadastrarEmpresa";
+		
+	}
+	
+	// LISTA EMPRESA
+	@RequestMapping("/empresas")
+	public ModelAndView listaEmpresa() {
+		
+		ModelAndView mv = new ModelAndView("empresa/lista-empresa");
+		Iterable<Empresa> empresas = er.findAll();
+		mv.addObject("empresas", empresas);
+		return mv;
+		
+	}
+	
+	// DELETA EMPRESA
+	@RequestMapping("/deletarEmpresa")
+	public String deletarEmpresa(long codigo) {
+		
+		Empresa empresa = er.findByCodigo(codigo);
+		er.delete(empresa);
+		return "redirect:/empresas";
+		
+	}
+	
+	// EDITAR EMPRESA
+	@RequestMapping(value = "editar-vaga", method = RequestMethod.GET)
+	public ModelAndView editarEmpresa(long codigo) {
+		
+		Empresa empresa = er.findByCodigo(codigo);
+		ModelAndView mv = new ModelAndView("empresa/update-empresa");
+		mv.addObject("empresa", empresa);
+		return mv;
+		
+	}
+	
+	// UPDATE EMPRESA
+	@RequestMapping(value = "/editar-empresa", method = RequestMethod.POST)
+	public String updateEmpresa(@Valid Empresa empresa, BindingResult result, RedirectAttributes attributes) {
+		
+		er.save(empresa);
+		attributes.addFlashAttribute("sucessos","Empresa alterada com sucesso");
+		
+		long codigoLong = empresa.getCodigo();
+		String codigo = "" +codigoLong;
+		return "redirect:/" + codigo;
+		
+	}
+	
+}
---
+++ AppRH/AppRH/src/main/java/com/AppRH/AppRH/models/Empresa.java
@@ -0,0 +1,88 @@
+package com.AppRH.AppRH.models;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+public class Empresa implements Serializable {
+	
+	// Controle de versionamento
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long codigo;
+	
+	@NotEmpty
+	private String nome;
+	
+	@NotEmpty
+	@Column(unique = true)
+	private String cnpj;
+	
+	@NotEmpty
+	private String email;
+	
+	@NotEmpty
+	private String telefone;
+	
+	@NotEmpty
+	private String responsavel;
+
+	public long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getResponsavel() {
+		return responsavel;
+	}
+
+	public void setResponsavel(String responsavel) {
+		this.responsavel = responsavel;
+	}
+
+}
